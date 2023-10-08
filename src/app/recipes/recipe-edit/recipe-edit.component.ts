fc60f4bf6605ed837b97b1a7ec5111382b0366e1
GururajKL <gururaj.kl@sasken.com>
2023-10-08 12:42:20 +0530

    Added Validation to recipe.

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipe.service';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -27,22 +28,76 @@ export class RecipeEditComponent implements OnInit {
     });
   }
 
+  onSubmit() {
+    const newRecipe = new Recipe(
+      this.recipeForm.get('name').value,
+      this.recipeForm.get('description').value,
+      this.recipeForm.get('imagePath').value,
+      this.recipeForm.get('ingredients').value
+    );
+
+    if (this.editMode) {
+      this.recipeService.updatedRecipe(this.id, newRecipe);
+    } else {
+      this.recipeService.addRecipe(newRecipe);
+    }
+  }
+
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        name: new FormControl(null, Validators.required),
+        amount: new FormControl(null, [
+          Validators.required,
+          Validators.pattern(/^[1-9]+[0-9]*$/),
+        ]),
+      })
+    );
+  }
+
+  /**
+   * Initialize the form using Reactive approach.
+   */
   private initForm() {
     let recipeName = '';
     let recipeImagePath = '';
     let recipeDescription = '';
+    let recipeIngredients = new FormArray([]); // Using form array for the ingredients to show.
 
     if (this.editMode) {
       const recipe = this.recipeService.getRecipeById(this.id);
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
+
+      // Check if current recipe has ingredients array in it.
+      if (recipe['ingredients']) {
+        // Add those as items as form control.
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name, Validators.required),
+              amount: new FormControl(ingredient.amount, [
+                Validators.required,
+                Validators.pattern(/^[1-9]+[0-9]*$/),
+              ]),
+            })
+          );
+        }
+      }
     }
 
+    // Creating new form with the formGroup.
     this.recipeForm = new FormGroup({
-      name: new FormControl(recipeName),
-      imagePath: new FormControl(recipeImagePath),
-      description: new FormControl(recipeDescription),
+      name: new FormControl(recipeName, Validators.required),
+      imagePath: new FormControl(recipeImagePath, Validators.required),
+      description: new FormControl(recipeDescription, Validators.required),
+      ingredients: recipeIngredients,
     });
+  }
+
+  get controls() {
+    const ingredientsArray = this.recipeForm.get('ingredients') as FormArray;
+    return ingredientsArray.controls;
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
+import { Subject } from 'rxjs';
 
 /**
  * Recipe Service returns the recipes and helps creating recipe.
  */
 @Injectable()
 export class RecipeService {
+  recipeChanged = new Subject<Recipe[]>();
+
   constructor(private shoppingListService: ShoppingListService) {}
 
   private recipes: Recipe[] = [
@@ -21,7 +24,7 @@ export class RecipeService {
       'A Test Recipe 2',
       'Simple test description.',
       'https://www.seriouseats.com/thmb/uH_msyHurzKTDRzc4c_goGoLANI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/SEA-classic-panzanella-salad-recipe-hero-03-74d7b17dde8f498795387ef0c22d7215.jpg',
-      [new Ingredient('Test 2', 1), new Ingredient('Test 3', 5)]
+      [new Ingredient('Tomatos', 1), new Ingredient('Chilli', 5)]
     ),
   ];
 
@@ -48,5 +51,24 @@ export class RecipeService {
    */
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.shoppingListService.addIngredients(ingredients);
+  }
+
+  /**
+   * Adds the recipe to the array.
+   * @param recipe Recipe object.
+   */
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
+  /**
+   * Updates the recipe item.
+   * @param index Position of the recipe need to update.
+   * @param newRecipe Updated recipe.
+   */
+  updatedRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipeChanged.next(this.recipes.slice());
   }
 }
