21fbd9dd2615b1595c157499d7fba187ebc8e839
GururajKL <gururaj.kl@sasken.com>
2023-10-17 15:24:05 +0530

    Optimize app with Lazy Loading.

---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list.component';
+
+@NgModule({
+  declarations: [ShoppingListComponent, ShoppingEditComponent],
+
+  imports: [
+    RouterModule.forChild([
+      { path: '', component: ShoppingListComponent },
+    ]),
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule,
+  ],
+})
+export class ShoppingListModule {}
